48f044776192d8d0e499a6dd73aa029c7e9a2e7d
Alex McDaniel <alexpmcdaniel@gmail.com>
2023-03-07 22:22:00 -0500

    lookup ranked info working

--- server/app.py
+++ server/app.py
@@ -28,35 +28,35 @@ def matches_from_puuid(puuid):
     # matches=cass.get_match_history(puuid=puuid, count=10, queue=cass.Queue.ranked_solo_fives, continent=cass.Region.north_america.continent)
     # thingy=[[json.loads(participant.to_json()) for participant in match.participants] for match in matches]
     # names=list(set(sum([[participant["summonerName"] for participant in matches] for matches in thingy],start=[])))
-    names=[
-"dundiseris",
-"FenisDeleter",
-"Antonellayg",
-"mynameisdamn",
-"fishstixz",
-"kaynis1",
-"xKratosx",
-"Canucklehead86",
-"peiliang ",
-"falserara",
-]
+    names=['projectmexican', 
+    'Egrillionyal', 
+    'BigBongRip', 
+    'CrackBracelet', 
+    'A Furry Smurf', 
+    'Pizza Pie Guy 23', 
+    'TicTacTfree', 
+    'BlackTeaCups', 
+    'Stylz Hazweed', 
+    'DamgeG00dz', 
+    'Paladin625', 
+    'Kamina Kiryuin', 
+    'TopVoliBEAR', 
+    'dontplayleagueL', 
+    'starbucker804', 
+    'CPT CRUNCH 69', 
+    'YouLeagueBruh']
 
-    allSoloQueueInfo=[]
+    allSoloFivesInfo=[]
     for name in names:
         try:
             summoner=cass.Summoner(name=name, region="NA")
-            # league = summoner.ranks[cass.Queue.ranked_solo_fives]
-            # print(league.divison, league.tier)
-            entries = []
-            test= [summoner.league_entries[cass.Queue.ranked_solo_fives]]
-            print(test.to_json())
-            #if queue is solo 5v5 add to dict
-            entries.append(test)
-            
-            print(test)
+            soloFivesData= summoner.league_entries[Queue.ranked_solo_fives].to_json()
+            allSoloFivesInfo.append(soloFivesData)
+
         except Exception as e:
             print(e)
-    return allSoloQueueInfo
-    # rankedTiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD','PLATINUM', 'DIAMOND', 'MASTER','GRANDMASTER','CHALLENGER']
-    # rankedRanks = ['IV', 'III', 'II', 'I']
-    # allSoloQueueInfo.sort(key=lambda x: (rankedTiers.index(x['tier']), rankedRanks.index(x.rank), x.leaguePoints))
+    print (allSoloFivesInfo)
+    rankedTiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD','PLATINUM', 'DIAMOND', 'MASTER','GRANDMASTER','CHALLENGER']
+    rankedRanks = ['IV', 'III', 'II', 'I']
+    allSoloFivesInfo.sort(key=lambda x: (rankedTiers.index(x['tier']), rankedRanks.index(x.rank), x.leaguePoints))
+    return allSoloFivesInfo
