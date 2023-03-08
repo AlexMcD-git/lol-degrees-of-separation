43d2499ad378bc89044982cbe248a6804882ba41
Alex McDaniel <alexpmcdaniel@gmail.com>
2023-03-08 03:23:51 -0500

    initial lookup changed to return same data

--- client/src/components/SummonerForm.js
+++ client/src/components/SummonerForm.js
@@ -4,97 +4,100 @@ function SummonerForm() {
     const [summonerName, setSummonerName] = useState('')
     const [summonerPuuid, setSummonerPuuid] = useState('')
     const [playerPath, setPlayerPath] = useState([])
-    let matchData = []
     const playerListPuuid = new Set()
-    let rankedInfo = []
 
-    function summonerLookup(name) {
+    function summonerLookup(name, region='NA') {
       fetch(`/puuid/${name}`)
       .then(r=>r.json())
       .then(data=>{
         setSummonerPuuid(data.puuid)
-        findChallenger(data.puuid)
-      }
-      )
+      })
     }
-    const tiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD','PLATINUM', 'DIAMOND', 'MASTER','GRANDMASTER','CHALLENGER'];
-    const ranks = ['IV', 'III', 'II', 'I'];
+    function findBestPlayerInRecentGames(name){
 
-    function findChallenger(puuid) {
-      fetch(`/matches/${puuid}`)
-        .then(r => r.json())
-        .then(matches => {
-          let index = 0;
-          function processMatch() {
-            if (index === matches.length) {
-              return;
-            }
+    }
+
+
+
+
     
-            fetch(`/match/${matches[index]}`)
-              .then(r => r.json())
-              .then(data => {
-                matchData.push(data);
-                console.log(matchData)
-                if (matchData.length === 10) {
-                  matchData.forEach(match => {
-                    match.forEach(participant => playerListPuuid.add(participant));
-                  });
-                  let playerListPuuidArray = [...playerListPuuid];
+    // const tiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD','PLATINUM', 'DIAMOND', 'MASTER','GRANDMASTER','CHALLENGER'];
+    // const ranks = ['IV', 'III', 'II', 'I'];
+    // function findChallenger(puuid) {
+    //   fetch(`/matches/${puuid}`)
+    //     .then(r => r.json())
+    //     .then(matches => {
+    //       let index = 0;
+    //       function processMatch() {
+    //         if (index === matches.length) {
+    //           return;
+    //         }
     
-                  let rankedInfoIndex = 0;
-                  function processPlayer() {
-                    if (rankedInfoIndex === playerListPuuidArray.length) {
-                      rankedInfo.sort((a, b) => {
-                        if (tiers.indexOf(a.tier) !== tiers.indexOf(b.tier)) {
-                          return tiers.indexOf(a.tier) - tiers.indexOf(b.tier);
-                        }
-                        if (ranks.indexOf(a.rank) !== ranks.indexOf(b.rank)) {
-                          return ranks.indexOf(a.rank) - ranks.indexOf(b.rank);
-                        }
-                        return a.leaguePoints - b.leaguePoints;
-                      });
-                      console.log(rankedInfo)
-                      let bestPlayer=rankedInfo[-1];
+    //         fetch(`/match/${matches[index]}`)
+    //           .then(r => r.json())
+    //           .then(data => {
+    //             matchData.push(data);
+    //             console.log(matchData)
+    //             if (matchData.length === 10) {
+    //               matchData.forEach(match => {
+    //                 match.forEach(participant => playerListPuuid.add(participant));
+    //               });
+    //               let playerListPuuidArray = [...playerListPuuid];
+    
+    //               let rankedInfoIndex = 0;
+    //               function processPlayer() {
+    //                 if (rankedInfoIndex === playerListPuuidArray.length) {
+    //                   rankedInfo.sort((a, b) => {
+    //                     if (tiers.indexOf(a.tier) !== tiers.indexOf(b.tier)) {
+    //                       return tiers.indexOf(a.tier) - tiers.indexOf(b.tier);
+    //                     }
+    //                     if (ranks.indexOf(a.rank) !== ranks.indexOf(b.rank)) {
+    //                       return ranks.indexOf(a.rank) - ranks.indexOf(b.rank);
+    //                     }
+    //                     return a.leaguePoints - b.leaguePoints;
+    //                   });
+    //                   console.log(rankedInfo)
+    //                   let bestPlayer=rankedInfo[-1];
                       
 
-                      setPlayerPath(current => [...current, `${bestPlayer.summonerName} (${bestPlayer.tier} ${bestPlayer.rank})`])
-                      matchData=[]
-                      rankedInfo=[]
-                      playerListPuuid.clear()
-                      if (bestPlayer.tier ==="CHALLENGER"){
-                        console.log("Wow it worked!")
-                        return
-                      }
-                      else{
-                        summonerLookup(bestPlayer.summonerName)
-                      }
-                      return;
-                    }
-                    fetch(`/rankedInfo/${playerListPuuidArray[rankedInfoIndex]}`)
-                      .then(r => r.json())
-                      .then(data => {
-                        console.log(data)
-                        const rankedSolo5x5 = data.filter(item=> item.queueType === 'RANKED_SOLO_5x5')
-                        if (rankedSolo5x5.length!==0){
-                        rankedInfo.push(rankedSolo5x5[0])
-                        console.log(rankedInfo);
-                      }
-                        rankedInfoIndex++;
-                        processPlayer()
-                        // setTimeout(processPlayer, 1000); // delay for 1 second before processing the next player
-                      });
-                  }
-                  processPlayer();
-                }
-                index++;
-                processMatch();
-                // setTimeout(processMatch, 1000); // delay for 1 second before processing the next match
-              });
-          }
-          processMatch();
-          console.log(playerPath)
-        })
-    }
+    //                   setPlayerPath(current => [...current, `${bestPlayer.summonerName} (${bestPlayer.tier} ${bestPlayer.rank})`])
+    //                   matchData=[]
+    //                   rankedInfo=[]
+    //                   playerListPuuid.clear()
+    //                   if (bestPlayer.tier ==="CHALLENGER"){
+    //                     console.log("Wow it worked!")
+    //                     return
+    //                   }
+    //                   else{
+    //                     summonerLookup(bestPlayer.summonerName)
+    //                   }
+    //                   return;
+    //                 }
+    //                 fetch(`/rankedInfo/${playerListPuuidArray[rankedInfoIndex]}`)
+    //                   .then(r => r.json())
+    //                   .then(data => {
+    //                     console.log(data)
+    //                     const rankedSolo5x5 = data.filter(item=> item.queueType === 'RANKED_SOLO_5x5')
+    //                     if (rankedSolo5x5.length!==0){
+    //                     rankedInfo.push(rankedSolo5x5[0])
+    //                     console.log(rankedInfo);
+    //                   }
+    //                     rankedInfoIndex++;
+    //                     processPlayer()
+    //                     // setTimeout(processPlayer, 1000); // delay for 1 second before processing the next player
+    //                   });
+    //               }
+    //               processPlayer();
+    //             }
+    //             index++;
+    //             processMatch();
+    //             // setTimeout(processMatch, 1000); // delay for 1 second before processing the next match
+    //           });
+    //       }
+    //       processMatch();
+    //       console.log(playerPath)
+    //     })
+    // }
 
     
     function handleSubmit(e){
--- server/app.py
+++ server/app.py
@@ -18,45 +18,42 @@ print(kalturi.id)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/puuid/<name>")
+@app.route("/intial-lookup/<name>")
 def puuid_from_name(name):
     summoner = cass.get_summoner(name=name, region="NA")
-    return {'puuid': summoner.puuid}
+    summonerData=json.loads(summoner.league_entries[Queue.ranked_solo_fives].to_json())
+    summonerData['puuid']=summoner.puuid
+    return summonerData
 
-@app.route("/matches/<puuid>")
+@app.route("/best-from-recent-games/<puuid>")
 def matches_from_puuid(puuid):
-    # matches=cass.get_match_history(puuid=puuid, count=10, queue=cass.Queue.ranked_solo_fives, continent=cass.Region.north_america.continent)
-    # thingy=[[json.loads(participant.to_json()) for participant in match.participants] for match in matches]
-    # names=list(set(sum([[participant["summonerName"] for participant in matches] for matches in thingy],start=[])))
-    names=['projectmexican', 
-    'Egrillionyal', 
-    'BigBongRip', 
-    'CrackBracelet', 
-    'A Furry Smurf', 
-    'Pizza Pie Guy 23', 
-    'TicTacTfree', 
-    'BlackTeaCups', 
-    'Stylz Hazweed', 
-    'DamgeG00dz', 
-    'Paladin625', 
-    'Kamina Kiryuin', 
-    'TopVoliBEAR', 
-    'dontplayleagueL', 
-    'starbucker804', 
-    'CPT CRUNCH 69', 
-    'YouLeagueBruh']
+    matches=cass.get_match_history(puuid=puuid, count=10, queue=cass.Queue.ranked_solo_fives, continent=cass.Region.north_america.continent)
+    thingy=[[json.loads(participant.to_json()) for participant in match.participants] for match in matches]
+    names=list(set(sum([[participant["summonerName"] for participant in matches] for matches in thingy],start=[])))
+    # names=['projectmexican', 
+    # 'Egrillionyal', 
+    # 'BigBongRip', 
+    # 'CrackBracelet', 
+    # 'A Furry Smurf', 
+    # 'Pizza Pie Guy 23', 
+    # 'TicTacTfree', 
+    # 'BlackTeaCups', 
+    # 'Stylz Hazweed']
 
     allSoloFivesInfo=[]
     for name in names:
         try:
             summoner=cass.Summoner(name=name, region="NA")
-            soloFivesData= summoner.league_entries[Queue.ranked_solo_fives].to_json()
+            soloFivesData= json.loads(summoner.league_entries[Queue.ranked_solo_fives].to_json())
             allSoloFivesInfo.append(soloFivesData)
 
         except Exception as e:
             print(e)
     print (allSoloFivesInfo)
     rankedTiers = ['IRON', 'BRONZE', 'SILVER', 'GOLD','PLATINUM', 'DIAMOND', 'MASTER','GRANDMASTER','CHALLENGER']
-    rankedRanks = ['IV', 'III', 'II', 'I']
-    allSoloFivesInfo.sort(key=lambda x: (rankedTiers.index(x['tier']), rankedRanks.index(x.rank), x.leaguePoints))
-    return allSoloFivesInfo
+    rankedDivision = ['IV', 'III', 'II', 'I']
+    allSoloFivesInfo.sort(reverse = True, key=lambda x: (rankedTiers.index(x['tier']), rankedDivision.index(x['division']), x['leaguePoints']))
+    bestPlayer=allSoloFivesInfo[0]
+    #saves looking up puuid for all players
+    bestPlayer['puuid']= cass.get_summoner(id=bestPlayer['summonerId'], region="NA").puuid
+    return bestPlayer
